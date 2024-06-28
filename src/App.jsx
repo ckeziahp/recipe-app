7b93cd9d0e9b82ed38f781b1e11312201d7859d2
ckeziahp <ckeziah95@gmail.com>
2024-06-28 09:30:45 +0000

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Welcome from "./pages/Welcome";
+import Recipes from "./pages/Recipes";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Categories from "./pages/Categories";
+import './App.css';
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <Welcome />},
+  {path: '/recipes', element: <Recipes />},
+  {path: '/categories', element: <Categories/>}
+])
 
 function App() {
   
 
   return (
     <>
-      <Welcome />
+      <RouterProvider router= {router}/>
+      
     
     </>
   )
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,8 +3,8 @@ import welcomeImage from "../assets/images/welcome-image.png";
 export default function welcome() {
 
     return (
-        <div>
-            <h3>Savory & Sweet</h3>
+        <div className="bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.5)),url('./assets/images/welcome-bg-image.png')] bg-cover bg-center h-screen flex flex-col place-items-center place-content-center">
+            <h3 className="text-white text-center text-4xl">Savory &<br></br> Sweet</h3>
             <img src={welcomeImage} alt="welcome Image" />
         </div>
     )
